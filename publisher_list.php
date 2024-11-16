5270659166841c4c5fe5789a9808ec364426c47c
Saujanya Poudel <saujanyapdl22@gmail.com>
2024-11-16 21:35:56 +0545

    Update publisher_list.php

--- publisher_list.php
+++ publisher_list.php
@@ -5,11 +5,11 @@
 
 	// Fetch publishers and count the number of books per publisher using JOIN and GROUP BY
 	$query = "
-		SELECT publisher.publisherid, publisher.publisher_name, COUNT(books.publisherid) AS book_count 
-		FROM publisher
-		LEFT JOIN books ON publisher.publisherid = books.publisherid
-		GROUP BY publisher.publisherid
-		ORDER BY publisher.publisherid
+		SELECT publishers.publisher_id, publishers.publisher_name, COUNT(books.publisherid) AS book_count 
+		FROM publishers
+		LEFT JOIN books ON publishers.publisher_id = books.publisherid
+		GROUP BY publishers.publisher_id
+		ORDER BY publishers.publisher_id
 	";
 	$result = mysqli_query($conn, $query);
 	if(!$result){
@@ -36,7 +36,7 @@
 		while($row = mysqli_fetch_assoc($result)){
 			// Display each publisher with book count
 	?>
-		<a class="list-group-item list-group-item-action" href="bookPerPub.php?pubid=<?php echo $row['publisherid']; ?>">
+		<a class="list-group-item list-group-item-action" href="bookPerPub.php?pubid=<?php echo $row['publisher_id']; ?>">
 			<span class="badge badge-primary bg-primary rounded-pill"><?php echo $row['book_count']; ?></span>
 			<?php echo $row['publisher_name']; ?>
 		</a>
