27bcd4d37db133eb182fb9532945fee2401856bb
Saujanya Poudel <saujanyapdl22@gmail.com>
2024-11-20 20:49:32 +0545

    Update bookPerPub.php

--- bookPerPub.php
+++ bookPerPub.php
@@ -1,67 +1,76 @@
 <?php
-	session_start();
-	require_once "./functions/database_functions.php";
-	// get pubid
-	if(isset($_GET['publisherid'])){
-		$pubid = $_GET['publisherid'];
-	} else {
-		echo "Wrong query! Check again!";
-		exit;
-	}
+session_start();
+require_once "./functions/database_functions.php";
 
-	// connect database
-	$conn = db_connect();
-	$publisherName = getPublisherName($conn, $pubid);
+// Get pubid from the URL parameter
+if(isset($_GET['publisherid'])){
+    $publisherid = $_GET['publisherid'];  // Changed from $pubid to $publisherid for consistency
+} else {
+    echo "Wrong query! Check again!";
+    exit;
+}
 
-	$query = "SELECT book_isbn, book_title, book_image, book_descr FROM books WHERE publisherid = '$publisherid'";
-	$result = mysqli_query($conn, $query);
-	if(!$result){
-		echo "Can't retrieve data " . mysqli_error($conn);
-		exit;
-	}
-	if(mysqli_num_rows($result) == 0){
-		echo "Empty books ! Please wait until new books coming!";
-		exit;
-	}
+// Connect to the database
+$conn = db_connect();
+$publisherName = getPublisherName($conn, $publisherid);  // Corrected variable name
 
-	$title = "Books Per Publisher";
-	require "./template/header.php";
+// SQL query to fetch books by publisherid
+$query = "SELECT book_isbn, book_title, book_image, book_descr FROM books WHERE publisherid = '$publisherid'";
+$result = mysqli_query($conn, $query);
+if(!$result){
+    echo "Can't retrieve data " . mysqli_error($conn);
+    exit;
+}
+
+if(mysqli_num_rows($result) == 0){
+    echo "No books available for this publisher! Please check back later.";
+    exit;
+}
+
+$title = "Books Per Publisher";
+require "./template/header.php";
 ?>
 <style>
-	.book-item .img-holder {
-		height: 20em;
-	}
-	.book-item:nth-child(even){
-		direction: rtl !important;
-	}
+    .book-item .img-holder {
+        height: 20em;
+    }
+    .book-item:nth-child(even){
+        direction: rtl !important;
+    }
 </style>
-	<nav aria-label="breadcrumb">
-		<ol class="breadcrumb">
-			<li class="breadcrumb-item"><a href="publisher_list.php" class="text-decoration-none text-muted fw-light">Publishers</a></li>
-			<li class="breadcrumb-item active" aria-current="page"><?php echo $pubName; ?></li>
-		</ol>
-	</nav>
-	<div id="pubBooks">
-	<?php while($row = mysqli_fetch_assoc($result)){
+
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="publisher_list.php" class="text-decoration-none text-muted fw-light">Publishers</a></li>
+        <li class="breadcrumb-item active" aria-current="page"><?php echo $publisherName; ?></li> <!-- Corrected to $publisherName -->
+    </ol>
+</nav>
+
+<div id="pubBooks">
+<?php 
+    while($row = mysqli_fetch_assoc($result)) { 
 ?>
-	<div class="row book-item mb-2">
-		<div class="col-md-3">
-			<div class="img-holder overflow-hidden">
-				<img class="img-top" src="./bootstrap/img/<?php echo $row['book_image'];?>">
-			</div>
-		</div>
-		<div class="col-md-9">
-			<h4><?php echo $row['book_title'];?></h4>
-			<hr>
-			<p class="truncate-5"><?= $row['book_descr'] ?></p>
-			<a href="book.php?bookisbn=<?php echo $row['book_isbn'];?>" class="btn btn-primary">Get Details</a>
-		</div>
-	</div>
+    <div class="row book-item mb-2">
+        <div class="col-md-3">
+            <div class="img-holder overflow-hidden">
+                <img class="img-top" src="./bootstrap/img/<?php echo $row['book_image']; ?>" alt="Book Image">
+            </div>
+        </div>
+        <div class="col-md-9">
+            <h4><?php echo $row['book_title']; ?></h4>
+            <hr>
+            <p class="truncate-5"><?= $row['book_descr'] ?></p>
+            <a href="book.php?bookisbn=<?php echo $row['book_isbn']; ?>" class="btn btn-primary">Get Details</a>
+        </div>
+    </div>
 <?php
-	}
+    }
 ?>
 </div>
+
 <?php
-	if(isset($conn)) { mysqli_close($conn);}
-	require "./template/footer.php";
+if(isset($conn)) { 
+    mysqli_close($conn); 
+}
+require "./template/footer.php";
 ?>
